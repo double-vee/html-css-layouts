d30fa18f0612f029f53f072f734d7052f93f298c
Weronika Kansy <weronika.kansy@gmail.com>
2021-08-31 13:43:59 +0200

    Navbar bounce effect added to the scroll event

--- edgeledger_website/app.js
+++ edgeledger_website/app.js
@@ -1,8 +1,20 @@
 const navbar = document.getElementById('navbar');
+let scrolled = false;
 
-const handleScroll = () =>
-  pageYOffset >= 100
-  ? navbar.classList.remove('top')
-  : navbar.classList.add('top');
+const handleScroll = () => {
+  if(pageYOffset >= 100) {
+    navbar.classList.remove('top');
+    if(!scrolled) {
+      navbar.style.transform = `translateY(-${navbar.offsetHeight}px)`;
+    }
+    setTimeout(function() {
+      navbar.style.transform = 'translateY(0)';
+      scrolled = true;
+    }, 200);
+  } else {
+    navbar.classList.add('top');
+    scrolled = false;
+  }
+}
 
 window.addEventListener('scroll', handleScroll);
