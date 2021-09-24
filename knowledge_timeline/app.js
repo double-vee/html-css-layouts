2059f82ece3d13240a590c313d696780b95dbd7e
Weronika Kansy <weronika.kansy@gmail.com>
2021-09-25 01:10:06 +0200

    Class 'show' added to each list item fully visible in the viewport; visibility of items determined with the getBoundingClientRect() method

--- knowledge_timeline/app.js
+++ knowledge_timeline/app.js
@@ -1,1 +1,22 @@
 const listItems = document.querySelectorAll('#timeline li');
+
+const isInViewport = element => {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const run = () =>
+  listItems.forEach((item) => {
+    if(isInViewport(item)) {
+      item.classList.add('show');
+    }
+  });
+
+  window.addEventListener('load', run);
+  window.addEventListener('resize', run);
+  window.addEventListener('scroll', run);
